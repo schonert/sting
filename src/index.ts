ed1db12a301bb6e1f17a3ebf4f928a4c1f4be16c
Stefan <sh@schonert.dk>
2018-06-16 12:32:44 +0200

    add st

--- src/index.ts
+++ src/index.ts
@@ -6,3 +6,6 @@ export { default as StingBase } from './sting-base.class';
 
 export * from './sting-notification.class';
 export { default as StingNotification } from './sting-notification.class';
+
+export * from './sting.tag';
+export { default as st } from './sting.tag';
